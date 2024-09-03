c432c5ccd97dd3c246bd93d8e22a42a0267f6de2
Justin Wu <justinytwu@gmail.com>
2024-09-02 23:54:38 -0700

    changed naming convention

--- src/app.js
+++ src/app.js
@@ -1,35 +1,34 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
-// const cors = require('cors');
-// const helmet = require('helmet');
-// const morgan = require('morgan');
-
-const machineRoutes = require('./routes/machineRoutes');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+// const routes = require('./routes'); // Import your route files
 
 const app = express();
 
-
 // Middleware
-// app.use(helmet());
-// app.use(bodyParser.json());
-// app.use(cors());
-// app.use(morgan('combined'));
+app.use(helmet()); // Security
+app.use(morgan('dev')); // Logging
+app.use(cors()); // Enable CORS
+app.use(bodyParser.json()); // Parse JSON bodies
+app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded bodies
+
+// API Routes
+/*
+app.use('/api/admin', routes.adminRoutes);
+app.use('/api/user', routes.userRoutes);
+app.use('/api/gym', routes.gymRoutes);
+app.use('/api/machine', routes.machineRoutes);
+app.use('/api/log', routes.logRoutes);
+
+// Error handling middleware
+app.use(require('./middlewares/error.middleware'));
+*/
 
 // Test route
 app.get('/', (req, res) => {
   res.json({ message: "Welcome to Qym backend!" });
 });
-
-
-// Routes
-// app.use('/api/machines', machineRoutes);
-
-// Error handling middleware
-/*
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
-*/
 
 module.exports = app;
