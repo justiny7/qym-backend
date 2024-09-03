c49c411318a69eabbdf53e201e4ff3db173ea9a4
Justin Wu <justinytwu@gmail.com>
2024-09-02 23:36:47 -0700

    update gitignore, commented code to simplify

--- src/app.js
+++ src/app.js
@@ -1,31 +1,35 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const helmet = require('helmet');
-const morgan = require('morgan');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+// const helmet = require('helmet');
+// const morgan = require('morgan');
 
 const machineRoutes = require('./routes/machineRoutes');
 
 const app = express();
 
+
 // Middleware
-app.use(helmet());
-app.use(bodyParser.json());
-app.use(cors());
-app.use(morgan('combined'));
+// app.use(helmet());
+// app.use(bodyParser.json());
+// app.use(cors());
+// app.use(morgan('combined'));
 
 // Test route
 app.get('/', (req, res) => {
   res.json({ message: "Welcome to Qym backend!" });
 });
 
+
 // Routes
-app.use('/api/machines', machineRoutes);
+// app.use('/api/machines', machineRoutes);
 
 // Error handling middleware
+/*
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
 });
+*/
 
 module.exports = app;
